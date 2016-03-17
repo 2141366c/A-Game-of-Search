f0d2427d3b54f559732c2594d7324dea398c20f8
em21 <emckenziee@ymail.com>
2016-03-17 00:05:43 +0000

    Basic leader board template.

--- zombie_project/register/urls.py
+++ zombie_project/register/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name = 'registration_form')]
+	url(r'^$', views.index, name = 'login')]
--- zombie_project/register/views.py
+++ zombie_project/register/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
+
 def index(request):
-	return render(request, 'register/registration_form.html')
+    return render(request, 'register/login_page.html')
 
 # Create your views here.
