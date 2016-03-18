4424ab778030436f7056af02f51f1ab8a8b98185
Em Mck <Em Mck>
2016-03-18 00:44:50 +0000

    ..

---
+++ zombie-game/.idea/zombie_project/zombieGame/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+from zombieGame import views
+
+urlpatterns = patterns('',
+        url(r'^$', views.index, name='index'),
+        url(r'^login/$', views.login, name='login'),
+        url(r'^profile/$', views.profile, name='profile'),
+        url(r'^game/$', views.game, name='game'),
+        url(r'^leaderboard/$', views.leaderboard, name='leaderboard'),
+        url(r'^register/$', views.register, name='register'),
+        url(r'^login/$', views.user_login, name='login'),
+        )
